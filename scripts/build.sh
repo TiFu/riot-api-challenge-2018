84b519277887bea9e70e1da750ff8145ec043ff2
Tino Fuhrmann <tino-fuhrmann@web.de>
2018-12-31 02:27:51 +0100

    Make scripts fail when one command fails

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
+set -e
 echo "2) Building all projects"
 for line in $(find . -name "package.json" -not -path "*/node_modules/*"); do 
     a=$PWD; 
-    echo "Installing modules in $(dirname $line)"; 
+    echo "Building in $(dirname $line)"; 
     cd $(dirname $line); 
     npm run build; 
     cd $a; 
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,4 @@
+set -e
 echo "1) Installing npm modules in all directories!"
 for line in $(find . -name "package.json" -not -path "*/node_modules/*" -not -path "*/.vscode/*"); do 
     a=$PWD; 
