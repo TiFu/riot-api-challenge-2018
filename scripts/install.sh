d2020a934ad24919fc102975535ef51c4f74d6e8
Tino Fuhrmann <tino-fuhrmann@web.de>
2019-01-01 03:07:57 +0100

    Ignore helper script compilation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 set -e
 echo "1) Installing npm modules in all directories!"
-for line in $(find . -name "package.json" -not -path "*/node_modules/*" -not -path "*/.vscode/*"); do 
+for line in $(find . -name "package.json" -not -path "*/node_modules/*" -not -path "*/helper-scripts/*" -not -path "*/.vscode/*"); do 
     a=$PWD; 
     echo "Installing modules in $(dirname $line)"; 
     cd $(dirname $line); 
