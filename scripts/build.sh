80b72edbd248196047728909b2d29b4fe3048f09
Tino Fuhrmann <tino-fuhrmann@web.de>
2019-01-01 03:10:38 +0100

    Right path to helper_scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 set -e
 echo "2) Building all projects"
-for line in $(find . -name "package.json"  -not -path "*/helper-scripts/*" -not -path "*/node_modules/*"); do 
+for line in $(find . -name "package.json"  -not -path "*/helper_scripts/*" -not -path "*/node_modules/*"); do 
     a=$PWD; 
     echo "Building in $(dirname $line)"; 
     cd $(dirname $line); 
