e4d503584d08aac0abc03f3187e67f0408f837b9
Tino Fuhrmann <tino-fuhrmann@web.de>
2019-01-24 04:26:07 +0100

    Fixed bug in legal notice Fixes #23

--- client/client/src/components/MainComponent.tsx
+++ client/client/src/components/MainComponent.tsx
@@ -73,7 +73,7 @@ class MainComponent extends React.Component<MainComponentProps & MainComponentAc
         let legal = null;
         if (this.state.showLegalInfo) {
             legal = <div className="alert alert-dismissable legal">
-                <button type="button" onClick={() => this.handleCloseLegal()} className="close close-legal" data-dismiss="alert" aria-label="Close">
+                <button type="button" onClick={() => this.handleCloseLegal()} className="close close-legal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
                 Trophy Hunter isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.                
